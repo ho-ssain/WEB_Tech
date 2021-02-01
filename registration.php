5c6f0c8447e73950018ce52cb318ddd77ef22e79
Hossain Kabir <awarehossain@gmail.com>
2021-02-01 13:28:15 +0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Exercise 1</title>
+</head>
+
+<body>
+    <h1>REGISTRATION</h1>
+    <form>
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td> <input type="text" id="name" name="name"></td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td> <input type="text" id="e" name="email"></td>
+        </tr>
+        <tr>
+            <td>User Name:</td>
+            <td> <input type="text" id="uname" name="uname"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td> <input type="password" id="p" name="pwd"></td>
+        </tr>
+        <tr>
+            <td>Confirm Password:</td>
+            <td> <input type="password" id="cp" name="cpwd"></td>
+        </tr>
+        <tr>
+            <td> 
+                Gender: <input type="radio" id="male" name="gender" value="male"> Male
+                <input type="radio" id="male" name="gender" value="female"> Female
+                <input type="radio" id="male" name="gender" value="other"> Other
+            </td>
+        </tr>
+
+        <tr>
+            <td> Date of Birth <input type="date" id="birthday" name="birthday">
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+            </td>
+        </tr>
+        
+    </table>
+    </form>
+
+    
+
+
+
+</body>
+</html>
